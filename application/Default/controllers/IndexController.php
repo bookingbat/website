0755fc2eed973ee94055d9d4aa4bbbabd6372d7d
josh <josh.ribakoff@gmail.com>
2013-06-20 21:23:33 -0400

    It creates a business' DB then runs the install.sql from the app

--- application/Default/controllers/IndexController.php
+++ application/Default/controllers/IndexController.php
@@ -6,7 +6,20 @@ class IndexController extends Zend_Controller_Action
     {
         $user = bootstrap::getInstance()->getUser();
         $this->view->isLoggedIn = $user['id'];
-    }
 
+        if($this->getRequest()->isPost()) {
+            $db = Zend_Registry::get('db');
+
+            $dbname_escaped = 'bookingbat_'.preg_replace('#[^a-zA-Z0-9]#','',$this->_getParam('business_name'));
+            try {
+                $db->query(sprintf('CREATE DATABASE `%s`',$dbname_escaped));
+            } catch(Exception $e) {
+                // it must already exist, just try to proceed.
+            }
+            `mysql --user=root $dbname_escaped < ../application/install.sql`;
+        }
+
+
+    }
 
 }
